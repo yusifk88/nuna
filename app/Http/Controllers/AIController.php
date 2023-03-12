7f98228394ee14493e1fe2ef8cd1299740d61deb
yusifk88 <amloyal4life@gmail.com>
2023-03-12 22:24:53 +0000

    fixed AI

--- app/Http/Controllers/AIController.php
+++ app/Http/Controllers/AIController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\StoryHistory;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -10,18 +11,42 @@ class AIController extends Controller
     public function loveStory()
     {
         $token = config("app.open_ai_key");
+        $token = config("app.open_ai_key");
         $res = Http::withHeaders([
             "authorization" => "Bearer " . $token
-        ])->post('https://api.openai.com/v1/completions', [
-            "model" => "text-davinci-003",
-            "prompt" => "suggest a short love poem for my engagement",
+        ])->post('https://api.openai.com/v1/chat/completions', [
+            "model" => "gpt-3.5-turbo",
+            "messages" => [["role" => "user", "content" => "suggest a short love poem for my engagement"]],
             "max_tokens" => 100,
-            "stop"=> ["<br>"]
+            "stop" => ["<br>"]
         ]);
+        $response = $res->json();
+        if (isset($response['choices'])) {
 
-        $answer = $res->json()['choices'][0];
+            $text = $response['choices'][0]['message']['content'];
 
-        $answer['text']=str_replace("\n","<br>",$answer['text']);
+            StoryHistory::create([
+                "story" => $text
+            ]);
+
+        } else {
+
+
+            $localStory = StoryHistory::inRandomOrder()->first();
+
+            if ($localStory) {
+
+                $text = $localStory->story;
+
+            } else {
+
+                $text = "Sorry i could not make up something for you";
+            }
+
+        }
+
+        $answer = str_replace("\n", "<br>", $text);
+
         return success_response($answer);
 
 
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\WeddingsController;
 use App\Repositories\Payswitch;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -30,7 +31,4 @@ Route::get("w/{tag}/contribute", [WeddingsController::class, "paymentPage"])->na
 Route::post("w/{id}/attending", [WeddingsController::class, "SaveRSV"])->name("reserve");
 Route::post("w/{id}/checkout", [WeddingsController::class, "initCheckout"])->name("initCheckout");
 
-//$checkout = Payswitch::initialize_collection(100, "ykatulie@gmail.com", Payswitch::getMaxID(), "https://mynunaa.com");
-//dd($checkout);
-//
-//
+
