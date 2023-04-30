92bcac3add2d963737dcb844deac38fd8e526b84
yusifk88 <amloyal4life@gmail.com>
2023-04-30 01:34:19 +0000

    added user seed

---
+++ deploy.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('repository', 'git@github.com:yusifk88/nuna.git');
+set("branch", "admin");
+
+add('shared_files', ['.env']);
+add('shared_dirs', ['storage']);
+add('writable_dirs', [
+    'bootstrap/cache',
+    'storage',
+    'storage/app',
+    'storage/app/public',
+    'storage/framework',
+    'storage/framework/cache',
+    'storage/framework/cache/data',
+    'storage/framework/sessions',
+    'storage/framework/views',
+    'storage/logs',
+]);
+
+set('log_files', 'storage/logs/*.log');
+
+// Hosts
+
+host('134.209.186.174')
+    ->set('remote_user', 'root')
+    ->set('deploy_path', '/var/www/html/admin');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
